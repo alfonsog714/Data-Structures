d3dba5e9fdc51a9ac3d29d916069806e87f25dcb
Alfonso <alfonso.garcia2324@gmail.com>
2019-09-18 13:44:18 -0700

    doing binary trees, the cogs in my head are starting to turn. I'm not too sure how it'll all work though

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,11 +9,28 @@ class BinarySearchTree:
     self.left = None
     self.right = None
 
+    """
+    Rules of inserting in the binary tree ----
+      1. If the current node you're trying to insert is larger than or equal to the parent node, it goes to the right.
+      2. If the current node you're trying to insert is less than the parent node, it goes to the left.
+    """
+
   def insert(self, value):
     """
     * `insert` adds the input value to the binary search tree, adhering to the rules of the ordering of elements in a binary search tree.
     """
-    pass
+    # Grab a hold of the current node
+    current_tree = self
+    # We need to loop through the tree
+    checking = True
+    while checking is True:
+      # If the passed in node's value is greater than the current one, set it to the right
+      if value >= current_tree.value:
+        current_tree.right = value
+      # If the passed in node's value is less than the current node, set it to the left
+      elif value < current_tree.value:
+        current_tree.left = value
+      # If the passed in value
 
   def contains(self, target):
     """
